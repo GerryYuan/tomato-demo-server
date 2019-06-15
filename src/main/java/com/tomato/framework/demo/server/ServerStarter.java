cd0e7d4c056daec6398ca6a1db6b2ea471e802b4
gerry <yuanguohua_7818@163.com>
2019-06-15 23:18:26 +0800

    完善zk注册中心

--- src/main/java/com/tomato/framework/demo/server/ServerStarter.java
+++ src/main/java/com/tomato/framework/demo/server/ServerStarter.java
@@ -6,6 +6,7 @@ import com.tomato.framework.demo.server.service.impl.HelloWorldImpl;
 import com.tomato.framework.plugin.rmi.register.Register;
 import com.tomato.framework.plugin.rmi.register.ZKRegister;
 import com.tomato.framework.plugin.rmi.server.RmiServer;
+import com.tomato.framework.plugin.rmi.server.Server;
 import java.rmi.Remote;
 import java.util.Map;
 
@@ -13,11 +14,11 @@ public class ServerStarter {
     
     public static void main(String[] args) throws Exception {
         //RMI 注册
-//        Register register = new RmiRegister(HelloWorld.class.getSimpleName(), new HelloWorldImpl());
+//        Register register = new RmiRegister(HelloWorld.class.getName(), new HelloWorldImpl());
         Map<String, Remote> map = ImmutableMap
-            .of(HelloWorld.class.getSimpleName(), new HelloWorldImpl());
-        Register register = new ZKRegister("127.0.0.1", 2181, map);
-        RmiServer rmiServer = new RmiServer(register);
+            .of(HelloWorld.class.getName(), new HelloWorldImpl());
+        Register register = new ZKRegister("127.0.0.1:2181" , map);
+        Server rmiServer = new RmiServer("rmi://127.0.0.1:18081", register);
         rmiServer.start();
 //        rmiServer.stop();
     }
